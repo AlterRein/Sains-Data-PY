d613c02904ba276169e7dfcc4978af09639e85a5
AlterRein <azzikrismuhammad@gmail.com>
2026-07-27 04:40:23 -0700

    menambah sesuatu

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ df = pd.DataFrame(
     },
     index= [1, 2, 3, 4]
 )
+repivot_df = df.pivot(index='Umur', columns='Skill', values='Nama')
+renamee_df = df.rename(columns = {'Skill' : 'Ability'})
 
 print(df)
+print("\n")
+print(renamee_df)
+print("\n")
+print(repivot_df.isnull())
+
+python_not_null = repivot_df[repivot_df['Python'].notnull()]
+print(python_not_null)
