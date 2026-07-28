367623b76130fbabc75c51a37a22410109229bb7
AlterRein <azzikrismuhammad@gmail.com>
2026-07-28 04:22:09 -0700

    belajar read csv dengan pandas

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 import pandas as pd
 
-df = pd.DataFrame(
-    {
-        "Nama" : ["Syfa", "Abdul", "Ibnu", "Santi"],
-        "Umur" : ["17", "19", "18", "18"],
-        "Skill" : ["Python", "Java", "C++", "C"]
-    },
-    index= [1, 2, 3, 4]
-)
-repivot_df = df.pivot(index='Umur', columns='Skill', values='Nama')
-renamee_df = df.rename(columns = {'Skill' : 'Ability'})
+df = pd.read_csv('customers-100.csv')
+df.head(3)
+df
+Out = df.to_csv('customers-100.csv')
 
-print(df)
-print("\n")
-print(renamee_df)
-print("\n")
-print(repivot_df.isnull())
-
-python_not_null = repivot_df[repivot_df['Python'].notnull()]
-print(python_not_null)
+print(Out)
